1c2f2e8efc11eac073fe34097951a76ee8116038
brookIsMyName <bura8016@gmail.com>
2026-05-27 20:33:42 +0200

    index.js was modified from local to global

--- server/index.js
+++ server/index.js
@@ -26,7 +26,7 @@ initGoogleStrategy();
 const app  = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "https://salviahealth.vercel.app", credentials: true }));
 
 app.use("/api/symptoms",  symptomRoutes);   // before express.json() — keep as you had it
 
